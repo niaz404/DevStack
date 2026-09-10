458a34e97c9cffd42a94988571b7b0afc90843b7
Niaz-Tan <nuddintanim@gmail.com>
2026-09-10 23:53:23 +0600

    chor: setup + novbar done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <div className="h-screen w-screen bg-background">
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+const Navbar = () => {
+  return (
+    <header className="bg-surface text-white flex px-[7%] py-[1%] justify-between items-center">
+      {/* logo */}
+      <div className="flex justify-center items-center gap-2">
+        <div className="gradient-logo font-extrabold h-8.75 w-8.75 flex justify-center items-center rounded">
+          DS
+        </div>
+        <div className="text-3xl font-bold">
+          Dev<span className="text-[#db2777]">Stack</span>
+        </div>
+      </div>
+      {/* navigation */}
+      <nav>
+        <ol className="flex justify-center items-center gap-8 text-xl">
+          <li className="cursor-pointer activePage">Home</li>
+          <li className="cursor-pointer">Technologies</li>
+          <li className="cursor-pointer">Projects</li>
+          <li className="cursor-pointer">About</li>
+          <li className="cursor-pointer">Contact</li>
+        </ol>
+      </nav>
+      <div className="flex justify-center items-center text-xl font-bold text-white/90">
+        <button className="px-4 rounded-3xl py-2">Sign In</button>
+        <button className="bg-[#db2777] px-4 rounded-3xl py-2">Sign Up</button>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
