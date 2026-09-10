78b46cf968c5b53eb4a5fe17876f989d1db06003
Niaz-Tan <nuddintanim@gmail.com>
2026-09-11 03:11:44 +0600

    improve logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,11 @@
 const Logo = () => {
   return (
-    <div className="flex justify-center items-center gap-2">
-      <div className="gradient-logo font-extrabold h-8.75 w-8.75 flex justify-center items-center rounded">
+    <div className="flex justify-center items-center gap-2.5">
+      <div className="gradient-logo font-extrabold h-9 w-9 flex justify-center items-center rounded-md text-sm">
         DS
       </div>
-      <div className="text-2xl font-bold">
+
+      <div className="text-[22px] font-bold">
         Dev<span className="text-[#db2777]">Stack</span>
       </div>
     </div>
